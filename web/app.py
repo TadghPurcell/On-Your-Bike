c2e1057a205d8229d84d8f6027de8ab3ffd4d267
Daniel O'Flynn <danielpoflynn@gmail.com>
2024-02-22 10:22:04 +0000

    DB connection

--- web/app.py
+++ web/app.py
@@ -1,6 +1,40 @@
-from flask import Flask
+from flask import Flask, g, jsonify
 
 app = Flask(__name__, static_url_path='')
+
+
+def connect_to_database():
+    engine = create_engine("mysql://{}:{}@{}:{}/{}".format(config.USER,
+                                                           config.PASSWORD,
+                                                           config.URI,
+                                                           config.PORT,
+                                                           config.DB), echo=True)
+    return engine
+
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = connect_to_database()
+    return db
+
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+
+@app.route("/available/<int:station_id>")
+def get_stations():
+    engine = get_db()
+    data = []
+    rows = engine.execute(
+        "SELECT available_bikes from  where number = {};".format(station_id))
+    for row in rows:
+        data.append(dict(row))
+    return jsonify(available=data)
 
 
 @app.route('/')
