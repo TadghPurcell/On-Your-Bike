b9d45e05c71f69e9f2855d65996e6737378b7c8b
Tadgh Purcell <tadghp@gmail.com>
2024-04-18 11:32:07 +0100

    Add rollback to scrapers

--- web/static_jcd_data.py
+++ web/static_jcd_data.py
@@ -9,41 +9,46 @@ from Database import Station
 # Sets options to read entire data frame
 pd.set_option("display.max_rows", None, "display.max_columns", None)
 
-with open('./se-group27-project/web/static/dbinfo.json') as f:
-    db_info = json.load(f)
-# URI and name
-BIKE_API_KEY = db_info['JCKey']
-STATIONS_URI = 'https://api.jcdecaux.com/vls/v1/stations'
-NAME = 'dublin'
+try:
+    with open('./se-group27-project/web/static/dbinfo.json') as f:
+        db_info = json.load(f)
+    # URI and name
+    BIKE_API_KEY = db_info['JCKey']
+    STATIONS_URI = 'https://api.jcdecaux.com/vls/v1/stations'
+    NAME = 'dublin'
 
-USER = db_info['dbConnection']['USER']
-PASSWORD = db_info['dbConnection']['PASSWORD']
-URI = db_info['dbConnection']['URI']
-PORT = db_info['dbConnection']['PORT']
-DB = db_info['dbConnection']['DB']
+    USER = db_info['dbConnection']['USER']
+    PASSWORD = db_info['dbConnection']['PASSWORD']
+    URI = db_info['dbConnection']['URI']
+    PORT = db_info['dbConnection']['PORT']
+    DB = db_info['dbConnection']['DB']
 
-jcDecaux_data = requests.get(
-    STATIONS_URI, params={'apiKey': BIKE_API_KEY, 'contract': NAME})
-jcDecaux_info = jcDecaux_data.json()
+    jcDecaux_data = requests.get(
+        STATIONS_URI, params={'apiKey': BIKE_API_KEY, 'contract': NAME})
+    jcDecaux_info = jcDecaux_data.json()
 
-# use pd.DataFrame because data is already an object
-df = pd.DataFrame(jcDecaux_info)
+    # use pd.DataFrame because data is already an object
+    df = pd.DataFrame(jcDecaux_info)
 
-engine = create_engine('mysql://{}:{}@{}:{}/{}'.format(USER, PASSWORD, URI, PORT, DB), echo=True)
+    engine = create_engine('mysql://{}:{}@{}:{}/{}'.format(USER, PASSWORD, URI, PORT, DB), echo=True)
 
-# Takes all classes that extends from base and creates them in the
-# database connects to engine and creates table for each class
-Base.metadata.create_all(bind=engine)
+    # Takes all classes that extends from base and creates them in the
+    # database connects to engine and creates table for each class
+    Base.metadata.create_all(bind=engine)
 
-Session = sessionmaker(bind=engine)
-session = Session()
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-for row in df.itertuples():
-    existing_station = session.query(Station).filter_by(
-        station_id=row.number).first()
-    if existing_station is None:
-        station = Station(row.number, row.name, row.address,
-                          row.position['lat'], row.position['lng'], row.banking)
-        session.add(station)
-session.commit()
-session.close()
+    for row in df.itertuples():
+        existing_station = session.query(Station).filter_by(
+            station_id=row.number).first()
+        if existing_station is None:
+            station = Station(row.number, row.name, row.address,
+                            row.position['lat'], row.position['lng'], row.banking)
+            session.add(station)
+    session.commit()
+except Exception as e:
+    print(f"An error occurred with the stations script (static_jdc_data): {e}")
+    session.rollback()
+finally:    
+    session.close()
--- web/weather_info_predictive.py
+++ web/weather_info_predictive.py
@@ -7,49 +7,54 @@ from Database import WeatherPredictive
 
 # Sets options to read entire data frame
 
-with open('./se-group27-project/web/static/dbinfo.json') as f:
-    db_info = json.load(f)
-WEATHER_API_KEY = db_info['weatherKey']
-USER = db_info['dbConnection']['USER']
-PASSWORD = db_info['dbConnection']['PASSWORD']
-URI = db_info['dbConnection']['URI']
-PORT = db_info['dbConnection']['PORT']
-DB = db_info['dbConnection']['DB']
+try:
+    with open('./se-group27-project/web/static/dbinfo.json') as f:
+        db_info = json.load(f)
+    WEATHER_API_KEY = db_info['weatherKey']
+    USER = db_info['dbConnection']['USER']
+    PASSWORD = db_info['dbConnection']['PASSWORD']
+    URI = db_info['dbConnection']['URI']
+    PORT = db_info['dbConnection']['PORT']
+    DB = db_info['dbConnection']['DB']
 
-# Weather
-# Weather URI
-WEATHER_URI = 'https://api.openweathermap.org/data/2.5/forecast'
-weather_data = requests.get(WEATHER_URI, params={
-                            "units": "metric", "lat": 53.344, "lon": -6.2672, "appid": WEATHER_API_KEY})
-weather_info = weather_data.json()
+    # Weather
+    # Weather URI
+    WEATHER_URI = 'https://api.openweathermap.org/data/2.5/forecast'
+    weather_data = requests.get(WEATHER_URI, params={
+                                "units": "metric", "lat": 53.344, "lon": -6.2672, "appid": WEATHER_API_KEY})
+    weather_info = weather_data.json()
 
-print(weather_info['list'][0]['dt'])
-engine = create_engine('mysql://{}:{}@{}:{}/{}'.format(USER, PASSWORD, URI, PORT, DB), echo=True)
+    print(weather_info['list'][0]['dt'])
+    engine = create_engine('mysql://{}:{}@{}:{}/{}'.format(USER, PASSWORD, URI, PORT, DB), echo=True)
 
-# Takes all classes that extends from base and creates them in the
-# database connects to engine and creates table for each class
-Base.metadata.create_all(bind=engine)
+    # Takes all classes that extends from base and creates them in the
+    # database connects to engine and creates table for each class
+    Base.metadata.create_all(bind=engine)
 
-Session = sessionmaker(bind=engine)
-session = Session()
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-session.query(WeatherPredictive).delete()
-session.commit()
+    session.query(WeatherPredictive).delete()
+    session.commit()
 
-# Create new weather row
-for i in range(len(weather_info['list'])):
-    updated_predictive_weather = WeatherPredictive(
-    weather_info['list'][i]['dt'],
-    weather_info['list'][i]['weather'][0]['main'], 
-    weather_info['list'][i]['weather'][0]['description'],
-    weather_info['list'][i]['main']['temp'],
-    weather_info['list'][i]['main']['feels_like'],
-    weather_info['list'][i]['main']['temp_min'],
-    weather_info['list'][i]['main']['temp_max'],
-    weather_info['list'][i].get('rain'),
-    weather_info['list'][i]['main']['humidity'],
-    weather_info['list'][i]['wind']['speed'],
-    weather_info['list'][i]['clouds']['all'])
-    session.add(updated_predictive_weather)
-session.commit()
-session.close()
+    # Create new weather row
+    for i in range(len(weather_info['list'])):
+        updated_predictive_weather = WeatherPredictive(
+        weather_info['list'][i]['dt'],
+        weather_info['list'][i]['weather'][0]['main'], 
+        weather_info['list'][i]['weather'][0]['description'],
+        weather_info['list'][i]['main']['temp'],
+        weather_info['list'][i]['main']['feels_like'],
+        weather_info['list'][i]['main']['temp_min'],
+        weather_info['list'][i]['main']['temp_max'],
+        weather_info['list'][i].get('rain'),
+        weather_info['list'][i]['main']['humidity'],
+        weather_info['list'][i]['wind']['speed'],
+        weather_info['list'][i]['clouds']['all'])
+        session.add(updated_predictive_weather)
+    session.commit()
+except Exception as e:
+    print(f"An error occurred with the predictive weather script (weather_info_predictive): {e}")
+    session.rollback()
+finally:    
+    session.close()
