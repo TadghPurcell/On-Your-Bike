f97e677aa1248c290d073524455cd4d0328958a7
Tadgh Purcell <tadghp@gmail.com>
2024-04-18 12:02:05 +0100

    Update create_engine statement

--- web/app.py
+++ web/app.py
@@ -29,11 +29,10 @@ DB = db_info['dbConnection']['DB']
 
 FORECAST_URI = 'https://api.openweathermap.org/data/2.5/forecast'
 
-engine = create_engine(
-    'mysql+pymysql://{}:{}@localhost:{}/{}'.format(USER, PASSWORD, PORT, DB), echo=True)
+
 # Create a new session
-# engine = create_engine(
-#     'mysql://{}:{}@{}:{}/{}'.format(USER, PASSWORD, URI, PORT, DB), echo=True)
+engine = create_engine(
+    'mysql://{}:{}@{}:{}/{}'.format(USER, PASSWORD, URI, PORT, DB), echo=True)
 
 Base.metadata.create_all(bind=engine)
 print("connected")
